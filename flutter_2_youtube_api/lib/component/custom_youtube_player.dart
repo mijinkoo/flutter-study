548b5374469066f8109099018f0fccbcf127d074
mijinkoo <starmj09@naver.com>
2023-01-14 11:41:02 +0900

    update

---
+++ flutter_2_youtube_api/lib/component/custom_youtube_player.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+import '../model/video_model.dart';
+// import 'package:youtube_player_flutter/';
---
+++ flutter_2_youtube_api/lib/restapi.dart
@@ -0,0 +1,3 @@
+// REST API
+// - REST API는 HTTP API로, GET/POST/PUT/DELETE 메서드 제공
+// - 플러터에서는 HTTP 요청으로 http 플러그인이나 dio 플러그인 사용
